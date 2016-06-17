ba18f3a3ea3c256990d8ed13bd40ff977f803608
Ovsyannikov Aleksey <insanus-mokrassar@yandex.ru>
2016-06-17 17:47:21 +0600

    added main logic, wait for details

--- DBInsertWithKeyTask/src/main/java/info/smart_tools/smartactors/core/db_insert_with_key_task/DBInsertWIthKeyTask.java
+++ DBInsertWithKeyTask/src/main/java/info/smart_tools/smartactors/core/db_insert_with_key_task/DBInsertWIthKeyTask.java
@@ -2,32 +2,89 @@ package info.smart_tools.smartactors.core.db_insert_with_key_task;
 
 import info.smart_tools.smartactors.core.idatabase_task.IDatabaseTask;
 import info.smart_tools.smartactors.core.idatabase_task.exception.TaskPrepareException;
+import info.smart_tools.smartactors.core.iioccontainer.exception.ResolutionException;
+import info.smart_tools.smartactors.core.iobject.FieldName;
 import info.smart_tools.smartactors.core.iobject.IObject;
+import info.smart_tools.smartactors.core.iobject.exception.ChangeValueException;
+import info.smart_tools.smartactors.core.iobject.exception.ReadValueException;
+import info.smart_tools.smartactors.core.ioc.IOC;
 import info.smart_tools.smartactors.core.itask.exception.TaskExecutionException;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class DBInsertWIthKeyTask implements IDatabaseTask {
 
     private IDatabaseTask targetTask;
     private String collectionName;
-    private String keyField;
 
-    private interface prepareQuery {
-    }
+    private String documentKey = "document";
+    private String startDateTimeKey = "startDateTime";
+    private String isActiveKey = "isActive";
+
+    private FieldName keyFieldName;
+    private FieldName documentKeyFieldName;
+    private FieldName startDateTimeFieldName;
+    private FieldName isActiveFieldName;
 
 
-    public DBInsertWIthKeyTask(IDatabaseTask targetTask, String collectionName, String keyField) {
+    private IObject objectForAdd = null;
+
+    public DBInsertWIthKeyTask(IDatabaseTask targetTask, String collectionName, String key) throws ResolutionException {
         this.targetTask = targetTask;
         this.collectionName = collectionName;
-        this.keyField = keyField;
+        keyFieldName = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), FieldName.class.toString()), key);
+        documentKeyFieldName = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), FieldName.class.toString()), documentKey);
+        startDateTimeFieldName = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), FieldName.class.toString()), startDateTimeKey);
+        isActiveFieldName = IOC.resolve(IOC.resolve(IOC.getKeyForKeyStorage(), FieldName.class.toString()), isActiveKey);
     }
 
+    /**
+     * Prepare object for adding into db
+     * @param query query object
+     *              <pre>
+     *              {
+     *                  "document"
+     *              }
+     *              </pre>
+     * @throws TaskPrepareException
+     */
     @Override
     public void prepare(IObject query) throws TaskPrepareException {
+
+        try {
+            IObject document = (IObject) query.getValue(documentKeyFieldName);
+            if (document.getValue(keyFieldName) == null) {//checking that keyFieldName is not empty
+                throw new TaskPrepareException("Document key is absent");
+            }
+
+            if (document.getValue(startDateTimeFieldName) == null) {
+                DateFormat dateFormat = DateFormat.getDateTimeInstance();
+                document.setValue(startDateTimeFieldName, dateFormat.format(new Date()));
+            }
+
+            targetTask.prepare(document);
+        } catch (ReadValueException | ChangeValueException e) {
+            throw new TaskPrepareException(e);
+        }
 
     }
 
     @Override
     public void execute() throws TaskExecutionException {
+        try {
+            Boolean isActiveOldState = (Boolean) objectForAdd.getValue(isActiveFieldName);
+            objectForAdd.setValue(isActiveFieldName, true);
 
+            try {
+                targetTask.execute();
+            } catch (TaskExecutionException e) {
+                objectForAdd.setValue(isActiveFieldName, isActiveOldState);
+                throw new TaskExecutionException(e);
+            }
+
+        } catch (ReadValueException | ChangeValueException e) {
+            throw new TaskExecutionException(e);
+        }
     }
 }
---
+++ DBInsertWithKeyTask/src/main/test/info/smart_tools/smartactors/core/db_insert_with_key_task/DBInsertWIthKeyTaskTest.java
@@ -0,0 +1,12 @@
+package info.smart_tools.smartactors.core.db_insert_with_key_task;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DBInsertWIthKeyTaskTest {
+    @Test
+    public void test() {
+        new DBInsertWIthKeyTask(null, null, "key");
+    }
+}
