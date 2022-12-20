3b5c367844209d41344f10ac55bdf29573a6f929
Alexander Ionov <alexander.ionov@7bits.it>
2022-12-20 17:51:02 +0600

    display user-friendly errors when validator fails

--- DevTools/Builder/src/main/java/info/smart_tools/smartactors/builder/validator/MavenValidator.java
+++ DevTools/Builder/src/main/java/info/smart_tools/smartactors/builder/validator/MavenValidator.java
@@ -4,17 +4,15 @@ import java.io.File;
 
 public class MavenValidator implements IValidator {
     @Override
-    public void validate(final String featurePath) throws ValidationException {
-        try {
-            new File(featurePath + "/pom.xml");
-        } catch (Exception e) {
-            throw new ValidationException("Unable to load pom.xml in " + featurePath, e);
+    public void validate(final String featurePath) {
+        if (!new File(featurePath + "/pom.xml").exists()) {
+            System.out.println("[\033[1;31mERROR\033[0m] Unable to load pom.xml in " + featurePath);
+            System.exit(-1);
         }
 
-        try {
-            new File(System.getenv("M2_HOME"));
-        } catch (Exception e) {
-            throw new ValidationException("Unable to read M2_HOME", e);
+        if (System.getenv("M2_HOME") == null) {
+            System.out.println("[\033[1;31mERROR\033[0m] Unable to read M2_HOME from environment. Make sure that M2_HOME is set properly");
+            System.exit(-1);
         }
     }
 }
